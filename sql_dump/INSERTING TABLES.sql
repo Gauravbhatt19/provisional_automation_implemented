56ee3ce69fae31bb0df24efd5076de8b004b7084
Gaurav Bhatt <gauravbhatt1924@gmail.com>
2019-10-28 14:21:48 +0530

    Working on third module

---
+++ sql_dump/INSERTING TABLES.sql
@@ -0,0 +1,6 @@
+#GENERAL TABLE
+
+INSERT INTO general(name,val,status) VALUES ('instructions','1.Kindly fill all your details in the form given in portal. (Applying)
+<br> 2. Once the details are filled , check the mail id which you provided. (Waiting Time)
+<br> 3.You shall receive a copy of the provisional marksheet on your mail id.(Receiving Soft Copy)
+<br> 4.Take a Print out of this and then get it signed from the Dy.Controller of Examination Division.(Validating)','1');
