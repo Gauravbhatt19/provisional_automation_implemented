f398b70370d1c789975bfd3740a3340961414992
Gaurav Bhatt <gauravbhatt1924@gmail.com>
2019-11-21 08:12:48 +0530

    All Error Resolved

--- kompiler_addmin_poortaal/generate/mails.php
+++ kompiler_addmin_poortaal/generate/mails.php
@@ -29,12 +29,13 @@ $filename=$id.'_'.$rollno;
 try {
  $mail->SMTPDebug =0;      
     $mail->isSMTP();   
-    $mail->Host       = 'smtp.gmail.com'; 
+    $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth   = true;    
     $mail->Username   = 'provisional.thdcihet@gmail.com';
     $mail->Password   = 'Aa1357908642';              
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;  
-    $mail->Port       = 587;                             
+    $mail->SMTPSecure = 'ssl';  
+    $mail->Host = 'ssl://smtp.gmail.com:465';
+    $mail->Port = 465;   
     $mail->setFrom('provisional.thdcihet@gmail.com', 'Provisional Portal');
     $mail->addAddress($email_id,$name);   
     $mail->addReplyTo('provisional.thdcihet@gmail.com', 'Provisional Portal');
